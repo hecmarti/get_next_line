74270eecdba3c47b575d28343c0c2ae21a6eb6cb
Hector Martinez Nunez <hecmarti@c3r8s1.42urduliz.com>
2024-04-25 17:11:24 +0200

    norma en gnl.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hecmarti <hecmarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 12:45:40 by hecmarti          #+#    #+#             */
-/*   Updated: 2024/04/25 16:03:07 by hecmarti         ###   ########.fr       */
+/*   Updated: 2024/04/25 17:09:56 by hecmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,12 @@ char	*linefree(char *line)
 
 /**
  * @brief Reads the next line from a file descriptor
- * specified by `fd`. It uses a static variable `rchars` to store
+ * specified by `fd`. It uses a static variable `r_ch` to store
  * the remaining characters from previous reads. The function
- * reads `BUFFER_SIZE` bytes at a time into a buffer, and appends it to `rchars`.
+ * reads `BUFFER_SIZE` bytes at a time into a buffer, and appends it to `r_ch`.
  * It continues reading until it encounters a newline character or reaches
- * the end of the file. The function then extracts the line from `rchars`,
- * updates `rchars` to store the remaining characters,
+ * the end of the file. The function then extracts the line from `r_ch`,
+ * updates `r_ch` to store the remaining characters,
  * and returns the extracted line.
  *
  * @param fd The file descriptor to read from.
@@ -41,29 +41,27 @@ char	*linefree(char *line)
 char	*get_next_line(int fd)
 {
 	int			bytes_read;
-	char		*read_buffer;
-	static char	*rchars = NULL;
+	char		*read_b;
+	static char	*r_ch = NULL;
 	char		*line;
 
 	if (fd < 0 || BUFFER_SIZE < 1)
 		return (NULL);
-	read_buffer = malloc(BUFFER_SIZE + 1);
-	if (!read_buffer)
+	read_b = malloc(BUFFER_SIZE + 1);
+	if (!read_b)
 		return (NULL);
 	bytes_read = 1;
-	while (!ft_strchr(rchars, '\n') && bytes_read > 0)
+	while (!ft_strchr(r_ch, '\n') && bytes_read > 0)
 	{
-		bytes_read = read(fd, read_buffer, BUFFER_SIZE);
-		if (bytes_read < 0) {
-		ft_bzero(rchars, ft_slen(rchars, '\0'));
-			return ( free(read_buffer), NULL);
+		bytes_read = read(fd, read_b, BUFFER_SIZE);
+		if (bytes_read < 0)
+			return (ft_bzero(r_ch, ft_slen(r_ch, '\0')), free(read_b), NULL);
+		read_b[bytes_read] = '\0';
+		r_ch = ft_strjoin(r_ch, read_b);
 	}
-		read_buffer[bytes_read] = '\0';
-		rchars = ft_strjoin(rchars, read_buffer);
-	}
-	linefree(read_buffer);
-	line = ft_linejoin(rchars);
-	rchars = ft_substr(rchars, ft_slen(rchars, '\n') + 1, ft_slen(rchars, 0));
+	linefree(read_b);
+	line = ft_linejoin(r_ch);
+	r_ch = ft_substr(r_ch, ft_slen(r_ch, '\n') + 1, ft_slen(r_ch, 0));
 	if (!bytes_read && *line == '\0')
 		return (free(line), NULL);
 	return (line);
